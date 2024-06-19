b6728e2d4e6f7c0300b7cf0bbe42b2cc7ed38202
nishant savaliya <savaliyanishant912@gmail.com>
2024-06-19 12:31:21 +0530

    update

---
+++ React JS/module-03-Advance-JS/calc/calc.js
@@ -0,0 +1,11 @@
+// clear when click on c button
+
+function clr() {
+      document.getElementById("result").value="";
+}
+
+// when press on button its value get in result
+
+function getResult(val) {
+      document.getElementById("result").value+=val;
+}

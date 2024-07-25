199c7c547dc0c15a9fe3c3c5d49cfb4777f374d0
nishant savaliya <savaliyanishant912@gmail.com>
2024-07-25 22:48:58 +0530

    update

---
+++ React JS/Module-03-React-JS/01-Whai is react js.js
@@ -0,0 +1,90 @@
+/*
+
+What is React js?
+
+      React js is library of javascript.
+      React js manage and develope by facebook engineer.
+      React js used to create UI and web application just like facebook | instagram | twitter | netflix | snapchat etc.
+      React js is used to create SPA (single page application).
+      React js is 10x fast load data on browser.
+      React js is component based.
+      React js components are reusables.
+      React js is based on modules and reused it.
+      React js create props and states and updated data also.
+      React js used some libraries and packages to create a web application.
+      React js is SEO friendly.
+      React js used life cycle to create any web application.
+
+      React js life cycle : 
+
+            1) initiaalizations
+            2) mounting
+            3) updating
+            4) unmounting
+
+      React js used to load webpage using virtual DOM.
+
+      Differance b/w DOM and virtual DOM.
+      
+            * DOM is slow to load content, when virtual DOM Faster than DOM.
+            * DOM load html content inside of javascript using document.getElementById("id-name");
+            * virtual DOM is used to load html content in react js using ReactDOM.cteateRoot(document.getElementById("id-name"));
+            * DOM is case sensitive and virtual DOM  is also case sensitive.
+      
+      React js used to load html in React components using jsx elements.
+
+            * jsx stand for javascript xml (xtensible markup language) load dynamic content of html.
+            * jsx used to access html elements in React js.
+      
+      React js componentsn is small peace of file i.e. saved inside of react using .js or .jsx extension.
+
+      React js components  is two types inside of React.
+
+            a) function components
+            b) class components
+
+      Differance b/w npm and npx
+
+            * npm stand for node package manager
+            * npm is depandancy  manager of React js
+            * npx stand for node package eXecute it means it is also create a React js app but it is faster than npm
+            * npx is create an eXecute files of React js app so it is faster than npm
+            
+
+      React js create any application or install any application with two types
+
+            (a) npm (node package manager)
+
+                  step-1 : download node js in your systems
+                           https://nodejs.org/en/download/prebuilt-installer
+
+                  step-2 : check version of node js
+                              node -v
+                              npm -v
+                              npx -v
+
+                  step-3 : npx create-react-app app-name
+
+                  step-4 : cd app-name
+
+                  step-5 : npm start
+
+                  step-6 : create a react web server url
+
+                              http://localhost:3000/
+
+                        you can  now view  demoapp in the browser.
+
+                        local : http://localhost:3000
+                        on your network : http://192.168.29.106:3000
+
+                        Note that the development build is not optimized.
+                        To create a production build, use npm run build.
+                        webpack compiled successfully.
+
+            (b) using npm with vite package
+            
+
+
+
+*/
